3243bc4cbe7651d32c9f717655b6ae91de0f6031
pishchulenok <64210553+sirejja@users.noreply.github.com>
2022-03-26 23:26:27 +0300

    echobot for VK
    echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,10 +1,11 @@
 import logging
 import os
-import vk_api
-
+import vk_api as vk
+import random
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 from google.cloud import dialogflow
+
 
 load_dotenv()
 VK_TOKEN = os.environ['VK_TOKEN']
@@ -14,20 +15,18 @@ DIALOGFLOW_PROJECT_ID = os.environ['DIALOGFLOW_PROJECT_ID']
 logger = logging.getLogger(__name__)
 
 
-def main():
-    vk_session = vk_api.VkApi(token=VK_TOKEN)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
 
+
+if __name__ == "__main__":
+    vk_session = vk.VkApi(token=VK_TOKEN)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
-
-
-if __name__ == '__main__':
-    main()
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
